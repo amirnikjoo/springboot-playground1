d36ac09c4c2b52a0ead9ee4d8959116fa57d9223
DESKTOP-JSFENL8\a.nikjoo <a.nikjoo@kian.digital>
2021-09-20 11:15:55 +0430

    Initial version of sample3 generated by generator-jhipster@7.2.0

---
+++ jHipster-test3/src/test/java/com/kian/test/ArchTest.java
@@ -0,0 +1,29 @@
+package com.kian.test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.kian.test");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.kian.test.service..")
+            .or()
+            .resideInAnyPackage("com.kian.test.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.kian.test.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
---
+++ jHipster-test3/src/test/java/com/kian/test/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.kian.test;
+
+import com.kian.test.Sample3App;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = Sample3App.class)
+public @interface IntegrationTest {
+}
